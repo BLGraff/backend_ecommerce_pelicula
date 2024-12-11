444b4df1c8eb5fbd22e94a451561eac56cc5cd0e
bgraff <admin@blgsoft.com.ar>
2024-12-11 01:14:22 -0300

    configurar rutas publicas y privadas con rol, autenticar token keycloack

---
+++ src/main/java/com/backend/pelicula/config/SecurityConfiguration.java
@@ -0,0 +1,81 @@
+package com.backend.pelicula.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.core.GrantedAuthorityDefaults;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity // Habilita el uso de @PreAuthorize y @PostAuthorize
+class SecurityConfiguration {
+
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+                .cors(withDefaults())
+                .csrf(AbstractHttpConfigurer::disable);
+
+        http.sessionManagement(sessionManagement ->
+                sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        );
+
+        http
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/pelicula/public/**").permitAll() // Permitir acceso anónimo
+                        .anyRequest().authenticated() // Requiere autenticación
+                )
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter()))
+                );
+
+        return http.build();
+    }
+
+    @Bean
+    GrantedAuthorityDefaults grantedAuthorityDefaults() {
+        return new GrantedAuthorityDefaults(""); // Remove the ROLE_ prefix
+    }
+
+    private JwtAuthenticationConverter jwtAuthenticationConverter() {
+        JwtAuthenticationConverter converter = new JwtAuthenticationConverter();
+
+        converter.setJwtGrantedAuthoritiesConverter(jwt -> {
+            var roles = extractRolesFromResourceAccess(jwt, "peliculas-api-rest");
+            return roles.stream()
+                    .map(role -> new SimpleGrantedAuthority(/*"ROLE_" + */role/*.toUpperCase()*/)) // Convertir a GrantedAuthority
+                    .collect(Collectors.toSet());
+        });
+
+        // converter.setJwtGrantedAuthoritiesConverter(authoritiesConverter);
+        return converter;
+    }
+
+    private Collection<String> extractRolesFromResourceAccess(Jwt jwt, String clientId) {
+        var resourceAccess = jwt.getClaimAsMap("resource_access");
+        if (resourceAccess == null || !resourceAccess.containsKey(clientId)) {
+            return Set.of(); // Sin roles si no existe el cliente
+        }
+
+        var clientRoles = (Map<String, Object>) resourceAccess.get(clientId);
+        return (Collection<String>) clientRoles.getOrDefault("roles", Set.of());
+    }
+
+}
--- src/main/java/com/backend/pelicula/pelicula/controller/PeliculaController.java
+++ src/main/java/com/backend/pelicula/pelicula/controller/PeliculaController.java
@@ -2,7 +2,11 @@ package com.backend.pelicula.pelicula.controller;
 
 import com.backend.pelicula.pelicula.dto.PeliculaDTO;
 import com.backend.pelicula.pelicula.service.PeliculaService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,16 +19,20 @@ public class PeliculaController {
     private PeliculaService peliculaService;
 
     @PostMapping
+    @ApiResponse(responseCode = "201")
+    @Operation(summary = "create  Test", security = @SecurityRequirement(name = "Bearer"))
+    @PreAuthorize("hasRole('admin')")
     public PeliculaDTO save(@RequestBody PeliculaDTO peliculaDTO) {
         return peliculaService.save(peliculaDTO);
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "create  Test", security = @SecurityRequirement(name = "Bearer"))
     public PeliculaDTO getPeliculaById(@PathVariable Long id) throws Exception {
         return peliculaService.findById(id);
     }
 
-    @GetMapping
+    @GetMapping("/public")
     public List<PeliculaDTO> getPelicula(
             @RequestParam(required = false) String titulo,
             @RequestParam(required = false) String genero
@@ -33,6 +41,8 @@ public class PeliculaController {
     }
 
     @PutMapping("/{id}")
+    @Operation(summary = "update  Test", security = @SecurityRequirement(name = "Bearer"))
+    @PreAuthorize("hasRole('admin')")
     public PeliculaDTO edit(@PathVariable Long id, @RequestBody PeliculaDTO peliculaDTO) throws Exception {
         return peliculaService.update(id, peliculaDTO);
     }
